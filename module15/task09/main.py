fd90ab23fffdb09da7ca0574b225174920fdd7de
George Kravtsov <george_kravtsov@mail.ru>
2021-12-22 11:57:33 +0700

    module15/task09/main.py fixed.

--- module15/task09/main.py
+++ module15/task09/main.py
@@ -1,7 +1,7 @@
 def check_palindrom(word):
     word_as_list = list(word)
 
-    for index in range(len(word_as_list)):
+    for index in range(len(word_as_list) // 2):
         back_index = -index - 1
         if word_as_list[index] != word_as_list[back_index]:
             return False
