ac1a879a143564f51db5126dc2f46e2dcf949b89
George Kravtsov <george_kravtsov@mail.ru>
2021-12-22 11:44:45 +0700

    module15/task09 done.

---
+++ module15/task09/main.py
@@ -0,0 +1,16 @@
+def check_palindrom(word):
+    word_as_list = list(word)
+
+    for index in range(len(word_as_list)):
+        back_index = -index - 1
+        if word_as_list[index] != word_as_list[back_index]:
+            return False
+
+    return True
+
+word = input('Введите слово: ')
+
+if check_palindrom(word):
+    print('Слово является палиндромом')
+else:
+    print('Слово не является палиндромом')
