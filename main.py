9750b8ef82a44b0c0fac2c93a352928d1025af77
George Kravtsov <george_kravtsov@mail.ru>
2021-12-27 10:33:25 +0700

    rename main to percent

--- main.py
+++ main.py
@@ -1,60 +1,25 @@
-films = [
-    'Крепкий орешек', 'Назад в будущее', 'Таксист',
-    'Леон', 'Богемская рапсодия', 'Город грехов',
-    'Мементо', 'Отступники', 'Деревня',
-    'Проклятый остров', 'Начало', 'Матрица'
-]
-favorite_films = []
-
-def film_in_list(film_name, film_list):
-    for i_film in film_list:
-        if i_film == film_name:
-            return True
-    return False
-
-def add_film(film_name):
-    if film_in_list(film_name, favorite_films) or not film_in_list(film_name, films):
-        return False
+def get_new_price(percent, price):
+    if percent == 0:
+        return price
     else:
-        favorite_films.append(film_name)
-        return True
-
-def remove_film(film_name):
-    if film_in_list(film_name, favorite_films):
-        favorite_films.remove(film_name)
-        return True
-    else:
-        return False
-
-def insert_film(film_name):
-    if film_in_list(film_name, favorite_films) or not film_in_list(film_name, films):
-        return False
-    else:
-        position = int(input('В какую позицию вставить? '))
-        favorite_films.insert(position - 1, film_name)
-        return True
-
-def print_error():
-    print('Ошибка.\nФильм должен быть в базе фильмов и его не должно быть в Вашем топе.')
-    print('Доступные команды: добавить, вставить, удалить, выход')
-
-def menu():
-    command = ''
-    while command != 'выход':
-        print('Ваш текущий топ фильмов:', favorite_films)
-        film_name = input('Название фильма: ')
-        print('Команды: добавить, вставить, удалить, выход')
-        command = input('Введите команду: ')
-
-        if command == 'добавить':
-            if not add_film(film_name):
-                print_error()
-        elif command == 'удалить':
-            if not remove_film(film_name):
-                print_error()
-        elif command == 'вставить':
-            if not insert_film(film_name):
-                print_error()
+        return round(price * (1 + percent / 100), 2)
 
 
-menu()
+price_list = [1.09, 23.56, 57.84, 4.56, 6.78]
+first_year_percent = 0
+second_year_percent = 10
+
+first_year_prices = [get_new_price(first_year_percent, price) for price in price_list]
+second_year_prices = [get_new_price(second_year_percent, price) for price in first_year_prices]
+
+print(first_year_prices)
+print(second_year_prices)
+print(round(sum(price_list), 2), round(sum(first_year_prices), 2), round(sum(second_year_prices), 2))
+
+
+
+
+
+
+
+# Сумма цен за каждый год: 93.83 93.83 103.22
