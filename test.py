16372b592752b35a31d2a1342a4e3b0525a434bd
George Kravtsov <george_kravtsov@mail.ru>
2021-12-27 13:49:35 +0700

    list comprehensions tests

--- test.py
+++ test.py
@@ -1,10 +1,18 @@
-left = 5
-right = 10
+import random
 
-cubes_list = [(left + x) ** 3 for x in range(right - left + 1)]
-square_list = [(left + x) ** 2 for x in range(right - left + 1)]
+min_dig = random.randint(0, 30)
+max_dig = random.randint(0, 30)
+work_list = [index for index in range(1, 30 + 1)]
+
+print('min = ', min_dig, '\tmax = ', max_dig)
+print('основной список', work_list)
+
+if min_dig > max_dig:
+    min_dig, max_dig = max_dig, min_dig
+
+print('min = ', min_dig, '\tmax = ', max_dig)
+
+work_list = [item for item in work_list if not min_dig <= item <= max_dig]
 
 
-print(cubes_list)
-print(square_list)
-print(sum(cubes_list))
+print('основной список после обработки', work_list)
