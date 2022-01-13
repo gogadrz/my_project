b1df252a795d568935f3cbe223dcdc44b941ba7f
George Kravtsov <george_kravtsov@mail.ru>
2022-01-14 00:52:59 +0700

    del test 21-4 added

---
+++ 21-4.py
@@ -0,0 +1,24 @@
+def create_dict(data):
+    if isinstance(data, dict):
+        return data
+    elif isinstance(data, int) or isinstance(data, float) or isinstance(data, str):
+        return {data: data}
+    else:
+        return None
+
+
+def data_preparation(old_list):
+    new_list = []
+
+    for i_element in old_list:
+        status = create_dict(i_element)
+        if status:
+            new_list.append(create_dict(i_element))
+
+    return new_list
+
+
+data = ['sad', {'sds': 23}, {43}, [12, 42, 1], 2323]
+print(data)
+data = data_preparation(data)
+print(data)
