0ab67ca8dd2c02d7763a74f3d3da587876d13f35
George Kravtsov <george_kravtsov@mail.ru>
2022-01-31 13:15:26 +0700

    Module25/task03 done.

--- Module25/03_my_dict/main.py
+++ Module25/03_my_dict/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+class MyDict(dict):
+
+    def get(self, *args):
+        if len(args) == 1:
+            return super(MyDict, self).get(args[0], 0)
+        else:
+            return super(MyDict, self).get(*args)
+
+
+some_dict = MyDict()
+some_dict['car'] = 'vaz'
+
+print(some_dict)
+
+print('Вызов get по умолчанию: {}'.format(some_dict.get('bike')))
+print('Вызов со вторым параметром : {}'.format(some_dict.get('bike', 'no')))
