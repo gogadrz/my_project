3d5b80737817d4d814c10905d414babed439ab20
George Kravtsov <george_kravtsov@mail.ru>
2022-02-02 01:45:37 +0700

    Module25/task06 done.

--- Module25/06_cohabitation_2/main.py
+++ Module25/06_cohabitation_2/main.py
@@ -7,7 +7,9 @@ def show_info(*args):
 
 
 def live_the_day(husband, wife, child, cats, house, report):
-    random_action = random.randint(1, 6)
+    # wife_action = random.randint(1, 6)
+    husband_action = random.randint(1, 4)
+    cats_action = random.randint(1, 2)
 
     house.set_dirt_state(5)
     if house.get_dirt_state() > 90:
@@ -16,96 +18,104 @@ def live_the_day(husband, wife, child, cats, house, report):
         child.set_happiness(-10)
 
     print('Что делали люди:')
+    print('----------------')
     # жена
-    if wife.get_ate_state() < 20:
-        wife.to_eat()
-        report['Съедено еды:'] += 25
-        print('{}\tпоела.'.format(wife.get_name()))
+    if wife.get_alive():
+        if wife.get_ate_state() < 25:
+            wife.to_eat()
+            report['Съедено еды:'] += 25
+            print('{wife:<10}поела.'.format(wife=wife.get_name()))
 
-    elif house.get_foods() < 50:
-        wife.buy_food()
-        print('{}\tсходила в магазин за продуктами.'.format(wife.get_name()))
+        elif house.get_foods() < 65:
+            wife.buy_food()
+            print('{wife:<10}сходила в магазин за продуктами.'.format(wife=wife.get_name()))
 
-    elif house.get_cat_foods() < 20:
-        wife.buy_food_for_cat()
-        print('{}\tкупила продукты для кота.'.format(wife.get_name()))
+        elif house.get_cat_foods() < 55:
+            wife.buy_food_for_cat()
+            print('{wife:<10}купила продукты для кота.'.format(wife=wife.get_name()))
 
-    elif house.get_dirt_state() > 80:
-        wife.clean_house(80)
-        print('{}\tсделала уборку.'.format(wife.get_name()))
+        elif house.get_dirt_state() > 80:
+            wife.clean_house(80)
+            print('{wife:<10}сделала уборку.'.format(wife=wife.get_name()))
 
-    elif house.get_money() > 300:
-        wife.buy_fur_coat()
-        report['Куплено шуб:'] += 1
-        print('{}\tкупила шубу!'.format(wife.get_name()))
+        elif house.get_money() > 300:
+            wife.buy_fur_coat()
+            report['Куплено шуб:'] += 1
+            print('{wife:<10}купила шубу!'.format(wife=wife.get_name()))
 
-    else:
-        wife.pet_the_cat()
-        print('{}\tпогладила кота.'.format(wife.get_name()))
+        else:
+            wife.pet_the_cat()
+            print('{wife:<10}погладила кота.'.format(wife=wife.get_name()))
 
     # муж
-    if husband.get_ate_state() < 20:
-        husband.to_eat()
-        report['Съедено еды:'] += 25
-        print('{}\tпоел.'.format(husband.get_name()))
+    if husband.get_alive():
+        if husband.get_ate_state() < 25:
+            husband.to_eat()
+            report['Съедено еды:'] += 25
+            print('{husband:<10}поел.'.format(husband=husband.get_name()))
 
-    elif house.get_money() < 50:
-        husband.go_to_work()
-        report['Заработано денег:'] += 150
-        print('{}\tсходил на работу.'.format(husband.get_name()))
+        elif house.get_money() < 50:
+            husband.go_to_work()
+            report['Заработано денег:'] += 150
+            print('{husband:<10}сходил на работу.'.format(husband=husband.get_name()))
 
-    elif random_action == 1 or random_action == 2:
-        husband.go_to_work()
-        report['Заработано денег:'] += 150
-        print('{}\tсходил на работу. (random action)'.format(husband.get_name()))
+        elif husband_action in range(1, 3):
+            husband.go_to_work()
+            report['Заработано денег:'] += 150
+            print('{husband:<10}сходил на работу. (random action)'.format(husband=husband.get_name()))
 
-    elif random_action == 3 or random_action == 4:
-        husband.to_eat()
-        report['Съедено еды:'] += 25
-        print('{}\tпоел. (random action)'.format(husband.get_name()))
-    else:
-        husband.play()
-        print('{}\tпоиграл в компьютер.'.format(husband.get_name()))
+        elif husband_action in range(4, 5):
+            husband.to_eat()
+            report['Съедено еды:'] += 25
+            print('{husband:<10}поел. (random action)'.format(husband=husband.get_name()))
+        else:
+            husband.play()
+            print('{husband:<10}поиграл в компьютер.'.format(husband=husband.get_name()))
 
     # ребенок
-    random_action = random.randint(1, 6)
-    if child.get_happiness() < 30:
-        child.sleep()
-        print('{}\tпоспал.'.format(child.get_name()))
+    random_action = random.randint(1, 7)
+    if child.get_alive():
+        if child.get_happiness() < 30:
+            child.sleep()
+            print('{child:<10}поспал.'.format(child=child.get_name()))
 
-    elif child.get_ate_state() < 20:
-        child.to_eat()
-        print('{}\tпокормили.'.format(child.get_name()))
+        elif child.get_ate_state() < 20:
+            child.to_eat()
+            print('{child:<10}поел.'.format(child=child.get_name()))
 
-    elif random_action == 1:
-        child.cry()
-        husband.set_happiness(-20)
-        wife.set_happiness(-20)
-        print('{}\tплакал.'.format(child.get_name()))
-    elif random_action == 2:
-        child.peed()
-        husband.set_happiness(-20)
-        wife.set_happiness(-20)
-        print('{}\tиспортил подгузник'.format(child.get_name()))
-    else:
-        child.play()
-        print('{}\tпоиграл'.format(child.get_name()))
+        elif random_action == 1:
+            child.cry()
+            husband.set_happiness(-20)
+            wife.set_happiness(-20)
+            print('{child:<10}плакал.'.format(child=child.get_name()))
+        elif random_action == 2:
+            child.peed()
+            husband.set_happiness(-20)
+            wife.set_happiness(-20)
+            print('{child:<10}испортил подгузник'.format(child=child.get_name()))
+        else:
+            child.play()
+            wife.set_happiness(20)
+            husband.set_happiness(10)
+            print('{child:<10}поиграл'.format(child=child.get_name()))
 
     print()
     print('Что делали коты:')
+    print('----------------')
     # коты
     for cat in cats:
-        if cat.get_eat_status() < 10:
-            cat.to_eat()
-            print('{}\tпоел.'.format(cat.get_name()))
+        if cat.get_alive():
+            if cat.get_eat_status() < 20:
+                cat.to_eat()
+                print('{cat:<10}поел.'.format(cat=cat.get_name()))
 
-        elif random.randint(1, 2) == 1:
-            cat.to_sleep()
-            print('{}\tпоспал.'.format(cat.get_name()))
+            elif cats_action == 1:
+                cat.to_sleep()
+                print('{cat:<10}поспал.'.format(cat=cat.get_name()))
 
-        else:
-            cat.scratch_wallpaper()
-            print('{}\tподрал обои.'.format(cat.get_name()))
+            else:
+                cat.scratch_wallpaper()
+                print('{cat:<10}подрал обои.'.format(cat=cat.get_name()))
 
     print()
 
@@ -153,7 +163,7 @@ class People:
         self.__home.append(home)
 
     def to_eat(self, value=25):
-        if self.__alive:
+        if self.__alive and self.__home[0].get_foods() > value:
             self.__home[0].set_foods(-value)
             self.__ate_state += value
 
@@ -166,19 +176,15 @@ class People:
         return self.__alive
 
     def info(self):
-        print('{name}\t'.format(name = self.__name), end = '')
+        print('{name:<10}'.format(name = self.__name), end = '')
         if self.__alive:
             print('Степень сытности: {ate_state}, Степень счастья: {happiness}'.format(
-                        ate_state=self.__ate_state,
-                        happiness=self.__happiness
+                ate_state=self.__ate_state,
+                happiness=self.__happiness
             ))
 
-            # if self.__alive:
-            #     print('Жизненный статус: жив')
-            # else:
-            #     print('Жизненный статус: умер')
         else:
-            print('- ' * 5, 'умер')
+            print('умер')
 
 
 class Husband(People):
@@ -201,22 +207,28 @@ class Child(People):
         super().__init__(name)
 
     def cry(self):
-        self.set_happiness(-5)
-        self.set_ate_state(-5)
+        if self.get_alive():
+            self.set_happiness(-5)
+            self.set_ate_state(-5)
 
     def peed(self):
-        self.set_happiness(-5)
-        self.set_ate_state(-5)
+        if self.get_alive():
+            self.set_happiness(-5)
+            self.set_ate_state(-5)
 
     def sleep(self):
-        self.set_happiness(10)
+        if self.get_alive():
+            self.set_happiness(10)
+            self.set_ate_state(-5)
 
     def to_eat(self, value=10):
-        super(Child, self).to_eat(value)
+        if self.get_alive():
+            super(Child, self).to_eat(value)
 
     def play(self):
-        self.set_happiness(10)
-        self.set_ate_state(-5)
+        if self.get_alive():
+            self.set_happiness(10)
+            self.set_ate_state(-5)
 
 
 class Wife(People):
@@ -257,6 +269,9 @@ class Cat:
     def get_eat_status(self):
         return self.__eat_status
 
+    def get_alive(self):
+        return self.__alive
+
     def get_name(self):
         return self.__name
 
@@ -265,12 +280,20 @@ class Cat:
 
     def to_eat(self, value=10):
         if self.__alive:
-            self.__eat_status += value * 2
-            self.__home[0].set_cat_foods(-value)
+            if self.__home[0].get_cat_foods() >= value:
+                self.__eat_status += value * 2
+                self.__home[0].set_cat_foods(-value)
+
+            else:
+                self.death()
+
             if self.__eat_status < 0:
-                self.__alive = False
-                self.__eat_status = 0
-                self.__home.clear()
+                self.death()
+
+    def death(self):
+        self.__alive = False
+        self.__eat_status = 0
+        self.__home.clear()
 
     def to_sleep(self):
         if self.__alive:
@@ -282,18 +305,15 @@ class Cat:
             self.__eat_status -= 10
 
     def info(self):
-        print('{name}\t'.format(name = self.__name), end = '')
+        print('{name:<10}'.format(name=self.__name), end='')
         if self.__alive:
             print('Степень сытности: {eat_state}'.format(
-                        name=self.__name,
-                        eat_state=self.__eat_status
+                name=self.__name,
+                eat_state=self.__eat_status
             ))
-            # if self.__alive:
-            #     print('Жизненный статус: Жив.')
-            # else:
-            #     print('Жизненный статус: Умер.')
+
         else:
-            print('- ' * 5, 'умер')
+            print('умер')
 
 
 class House:
@@ -328,8 +348,9 @@ class House:
         self.__money_in_nightstand += value
 
     def info(self):
-        print('\nДеньги в тумбочке: {money}, Еда в холодильнике: {food}, Кошачья еда: {pet_food}, '
-              'Количество грязи: {dust_level}'.format(
+        print('\n{home:<10}Деньги: {money}, Еда: {food}, Кошачья еда: {pet_food}, '
+              'Грязь: {dust_level}'.format(
+                    home='Дом',
                     money=self.__money_in_nightstand,
                     food=self.__foods_in_cool_box,
                     pet_food=self.__cat_food,
@@ -347,42 +368,47 @@ def main():
     days = 365
     house = House()
 
-    husband = Husband('Karl')
+    husband = Husband('Муж')
     husband.add_home(house)
 
-    wife = Wife('Klara')
+    wife = Wife('Жена')
     wife.add_home(house)
 
-    child = Child('vitalik')
+    child = Child('Ребенок')
     child.add_home(house)
 
     cats = list()
 
-    cat = Cat('Garfield')
+    cat = Cat('Гав')
     cat.add_home(house)
     cats.append(cat)
 
-    cat2 = Cat('Matvey')
+    cat2 = Cat('Том')
     cat2.add_home(house)
     cats.append(cat2)
 
-    cat3 = Cat('Tom')
+    cat3 = Cat('Пух')
     cat3.add_home(house)
     cats.append(cat3)
 
     for day in range(1, days + 1):
-        print('-' * 50)
+        print('=' * 59)
         print('День {}\n'.format(day))
         live_the_day(husband, wife, child, cats, house, life_report)
 
+        print('Информационный блок:')
+        print('-' * 20)
         show_info(wife, husband, child)
         print()
         show_info(cat, cat2, cat3, house)
 
-    print('\nИтого зa {} дней:'.format(days))
+    print('\nИтого зa {days} дней:'.format(days=days))
 
     for key, value in life_report.items():
-        print('{key} {value}'.format(key=key, value=value))
+        print('{key:<17} {value:>6}'.format(
+            key=key,
+            value=value
+        ))
 
 
 main()
