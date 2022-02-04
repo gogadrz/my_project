aeffab06a4e3626b92eb0fa98b30752c6f601d64
George Kravtsov <george_kravtsov@mail.ru>
2022-02-04 10:56:00 +0700

    Module26/task02 done.

--- Module26/02_refactoring/main.py
+++ Module26/02_refactoring/main.py
@@ -1,17 +1,23 @@
-list_1 = [2, 5, 7, 10]
-list_2 = [3, 8, 4, 9]
-to_find = 56
+from collections.abc import Iterable
 
-can_continue = True
-for x in list_1:
-    for y in list_2:
-        result = x * y
+
+def seek(looking_number: int) -> Iterable[int, int, int]:
+    list_1 = [2, 5, 7, 10]
+    list_2 = [3, 8, 4, 9]
+
+    for x in list_1:
+        for y in list_2:
+            result = x * y
+            yield x, y, result
+            if result == looking_number:
+                print('Found!!!')
+                return
+
+
+def main():
+    value = seek(looking_number=56)
+    for x, y, result in value:
         print(x, y, result)
-        if result == to_find:
-            print('Found!!!')
-            can_continue = False
-            break
-    if not can_continue:
-        break
 
-# TODO провести рефакторинг кода
+
+main()
