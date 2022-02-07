453eb725bae21ec6f70e100461920351e50d39e5
George Kravtsov <george_kravtsov@mail.ru>
2022-02-07 23:43:43 +0700

    Module27/task02 done.

--- Module27/02_slowdown/main.py
+++ Module27/02_slowdown/main.py
@@ -1,1 +1,39 @@
-# TODO здесь писать код
+from typing import Callable, Any
+import functools
+import time
+
+
+def timer_decorator(func: Callable) -> Callable:
+    """
+    Декоратор.
+
+    Перед вызовом декорируемой функции ждет 3 секунды.
+    """
+
+    @functools.wraps(func)
+    def wrapped_func(*args, **kwargs) -> Any:
+
+        print('Wait 3 seconds')
+        for _ in range(3):
+            time.sleep(1)
+            print('.', end='')
+        print('\nDone.')
+
+        value = func(*args, **kwargs)
+
+        return value
+
+    return wrapped_func
+
+
+@timer_decorator
+def my_function():
+    print('Inside my_function()')
+
+
+def main():
+
+    my_function()
+
+
+main()
