9c405a553164b415fdbaa603aa6ce9ac14c369e9
George Kravtsov <george_kravtsov@mail.ru>
2022-02-10 14:11:48 +0700

    Module28/task04 fixed.

--- Module28/04_date/main.py
+++ Module28/04_date/main.py
@@ -6,9 +6,10 @@ class Date:
     Класс Date
     """
 
-    def __init__(self, date_str: str) -> None:
-        date = self.from_string(date_str)
-        self.__f_date = datetime.date(date.year, date.month, date.day)
+    def __init__(self, day: int, month: int, year: int) -> None:
+        self.__day = day
+        self.__month = month
+        self.__year = year
 
     @classmethod
     def is_date_valid(cls, date_str: str) -> bool:
@@ -21,31 +22,22 @@ class Date:
         return True
 
     @classmethod
-    def from_string(cls, date_str: str) -> datetime.date:
-        return datetime.datetime.strptime(date_str, '%d-%m-%Y')
+    def from_string(cls, date_str: str) -> 'Date':
+        result = datetime.datetime.strptime(date_str, '%d-%m-%Y')
+        return Date(result.day, result.month, result.year)
 
     def __str__(self) -> str:
         return 'День: {day}\tМесяц: {month}\tГод: {year}'.format(
-            day=self.__f_date.day,
-            month=self.__f_date.month,
-            year=self.__f_date.year
+            day=self.__day,
+            month=self.__month,
+            year=self.__year
         )
 
 
 def main():
-    # Александр. Подскажите пожалуйста.
-    # Я никак не могу сделать что бы сработала
-    # строка из примера:
-    #     date = Date.from_string('10-12-2077')
-    # У меня работает вариант вызова конструктора,
-    # а из него вызывается метод from_string.
-    # Я не понимаю эту конструкцию, вызов метода без вызова конструктора...
 
-    # Во 2-ой задаче, с математическим модулем у меня возникла
-    # такая же проблема. Пришлось использовать @classmethod.
-
-    # date = Date.from_string('10-12-2077')
-    date = Date('10-12-2077')
+    date = Date.from_string('10-12-2077')
+    # date = Date(10, 12, 2077)
     print(date)
     print(Date.is_date_valid('10-12-2077'))
     print(Date.is_date_valid('40-12-2077'))
