7d782bc83bcedd38fc2dc203c537d93d0c16c183
George Kravtsov <george_kravtsov@mail.ru>
2022-02-14 22:09:08 +0700

    Module30/task01 done.

--- Module30/01_new_lists/main.py
+++ Module30/01_new_lists/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+from typing import List
+from functools import reduce
+
+floats: List[float] = [12.3554, 4.02, 5.777, 2.12, 3.13, 4.44, 11.0001]
+names: List[str] = ["Vanes", "Alen", "Jana", "William", "Richards", "Joy"]
+numbers: List[int] = [22, 33, 10, 6894, 11, 2, 1]
+
+print('Первый список: ', list(map(lambda x: round(x ** 3, 3), floats)))
+
+print('Второй список: ', list(filter(lambda y: len(y) >= 5, names)))
+
+print('Третий список: ', [reduce(lambda a, b: a * b, [i for i in numbers])])
