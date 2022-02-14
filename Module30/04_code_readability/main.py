9e8862e6633178e5d60b7d3b5fdcfad2c10fe4e6
George Kravtsov <george_kravtsov@mail.ru>
2022-02-15 00:35:39 +0700

    Module30/task04 draft

--- Module30/04_code_readability/main.py
+++ Module30/04_code_readability/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+def simple_digits(limit):
+    for index in range(2, limit):
+        if not index % 2 and not index  
+        print(index)
+
+
+
+simple_digits(100)
+# print([x for x in range(2, 1000) if not [t for t in range(2, x) if not x % t]])
