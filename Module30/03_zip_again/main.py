62ca9e298c0985bf24b88cf9f3596d6194ab51e9
George Kravtsov <george_kravtsov@mail.ru>
2022-02-15 00:14:26 +0700

    Module30/task03 done.

--- Module30/03_zip_again/main.py
+++ Module30/03_zip_again/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+from typing import List
+
+
+strings: List[str] = ['a', 'b', 'c', 'd', 'e']
+numbers: List[int] = [1, 2, 3, 4, 5, 6, 7, 8]
+
+results = [(strings[i], numbers[i]) for i in range(min(len(strings), len(numbers)))]
+print(results)
