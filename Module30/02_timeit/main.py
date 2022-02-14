a462b206503b95d778a58160f724250c4b041363
George Kravtsov <george_kravtsov@mail.ru>
2022-02-15 00:00:14 +0700

    Module30/task02 done.

--- Module30/02_timeit/main.py
+++ Module30/02_timeit/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+import timeit
+
+res: float = timeit.timeit('"-".join(str(n) for n in range(100))', number=10000)
+print(res)
+
+
+res_comprehensions: float = timeit.timeit("[''.join(str(i) for i in range(100))]", number=10000)
+print(res_comprehensions)
+
+
+res_map: float = timeit.timeit("[''.join(map(str, [a for a in range(100)]))]", number=10000)
+print(res_map)
