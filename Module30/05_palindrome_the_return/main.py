501b68a8264370be9281f2b847423037bfee77ba
George Kravtsov <george_kravtsov@mail.ru>
2022-02-15 09:00:14 +0700

    Module30/task05 draft

--- Module30/05_palindrome_the_return/main.py
+++ Module30/05_palindrome_the_return/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+def can_be_poly(in_string: str) -> bool:
+    return True
+
+
+def main() -> None:
+    print(can_be_poly('ababc'))
+    print(can_be_poly('abbbc'))
+
+
+if __name__ == '__main__':
+    main()
