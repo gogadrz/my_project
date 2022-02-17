471784da46d45bb9cdb1d558052f724f7451726a
George Kravtsov <george_kravtsov@mail.ru>
2022-02-17 23:53:59 +0700

    Module31/task06 done

--- Module31/06_web_scrapping/main.py
+++ Module31/06_web_scrapping/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+import requests
+import json
+import re
+
+
+def main() -> None:
+
+    url: str = 'http://www.columbia.edu/~fdc/sample.html'
+
+    my_req = requests.get(url)
+    data = my_req.text
+
+    result = re.findall(r'<h3.*>(.*)</h3>', data)
+
+    print(json.dumps(result, indent=4))
+
+
+if __name__ == '__main__':
+    main()
