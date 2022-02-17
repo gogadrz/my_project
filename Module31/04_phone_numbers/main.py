b24ef1807ba92b5741106a35cf990786156cea47
George Kravtsov <george_kravtsov@mail.ru>
2022-02-17 22:43:59 +0700

    Module31/task04 done

--- Module31/04_phone_numbers/main.py
+++ Module31/04_phone_numbers/main.py
@@ -1,11 +1,32 @@
 import re
 
 
-def main():
+def check_phone_number(number: str) -> bool:
+    if re.search(r'[89]\d{9}', number):
+        return True
+    else:
+        return False
 
-    phone_numbers_list: list = ['9999999999', '999999-999', '99999x9999']
 
-    result = re.
+def main() -> None:
+
+    phone_numbers_list: list[str] = ['9999999999',
+                                     '999999-999',
+                                     '99999x9999',
+                                     '8567467563',
+                                     '9-234-2345',
+                                     'd-082364293',
+                                     '8123456789'
+                                     ]
+
+    for index, number in enumerate(phone_numbers_list):
+        print('{index} номер: '.format(index=index + 1), end='')
+
+        if check_phone_number(number):
+            print('всё в порядке')
+        else:
+            print('не подходит')
+
 
 if __name__ == '__main__':
     main()
