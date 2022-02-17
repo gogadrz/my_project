decba44aec3b0625411b545be64e4ba75d33fbfe
George Kravtsov <george_kravtsov@mail.ru>
2022-02-17 19:04:33 +0700

    Module31/task04 draft

--- Module31/04_phone_numbers/main.py
+++ Module31/04_phone_numbers/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+import re
+
+
+def main():
+
+    phone_numbers_list: list = ['9999999999', '999999-999', '99999x9999']
+
+    result = re.
+
+if __name__ == '__main__':
+    main()
