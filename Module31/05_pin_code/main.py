d48a9106846e25108bca6670a5e763056e11e770
George Kravtsov <george_kravtsov@mail.ru>
2022-02-18 09:13:08 +0700

    itertools

--- Module31/05_pin_code/main.py
+++ Module31/05_pin_code/main.py
@@ -1,6 +1,16 @@
+import itertools
+
+
 def main() -> None:
-    for code in range(10000):
-        print('{:04d}'.format(code))
+    # for code in range(10000):
+    #     print('{:04d}'.format(code))
+
+    # лучше так
+    digits = range(10)
+    pincode_vars = itertools.product(digits, repeat = 4)
+
+    for var in pincode_vars:
+        print(var)
 
 
 if __name__ == '__main__':
